ce9c02280010be5303b03aac792dce3e3fe2a30b
Umar Niazi <uhkniazi@gmail.com>
2017-07-06 11:54:31 +0100

    testing a 3 component distribution

--- mercedezBenzGrMan/reduceModel.R
+++ mercedezBenzGrMan/reduceModel.R
@@ -109,6 +109,139 @@ iAggregate = rowSums(iAggregate)
 iMSE.val = mean((iAggregate - dfData.val$y)^2)
 
 #### refit the model again but on a different and larger subset of the data to see if error rate improves
+dfData = dfData.train.rf1
+dim(dfData)
+## take a subset of the training data to fit model
+i = sample(1:nrow(dfData), size = nrow(dfData)*0.80, replace = F)
+dfData.pr = dfData[-i,]
+dfData = dfData[i,]
+## make model matrix and fit model
+m = model.matrix(y ~ ., data=dfData)
+dim(m)
+
+lStanData = list(Ntotal=nrow(dfData), y=dfData$y, iMixtures=2, Ncol=ncol(m), X=m)
+
+## give initial values if you want, look at the density plot 
+initf = function(chain_id = 1) {
+  list(mu = c(90, 120), sigma = c(11, 11*2), iMixWeights=c(0.5, 0.5), nu=c(3, 3), betasMix1=iModel.1[-1],
+       betasMix2=iModel.2[-1])
+} 
+
+fit.stan2 = sampling(stanDso, data=lStanData, iter=1200, chains=1, init=initf, cores=1, pars=c('sigma', 'iMixWeights', 'betasMix1',
+                                                                                              'betasMix2', 'mu', 'nu'))
+print(fit.stan2, digi=3)
+#traceplot(fit.stanTMix)
+
+## get fitted values
+m = extract(fit.stan2)
+names(m)
+
+## get the coefficients
+iModel.1 = c(mean(m$mu[,1]), apply(m$betasMix1, 2, mean))
+iModel.2 = c(mean(m$mu[,2]), apply(m$betasMix2, 2, mean))
+iMixWeights = apply(m$iMixWeights, 2, mean)
+
+## calculate training error
+iPred1 = model.matrix(y ~ ., data=dfData) %*% iModel.1
+iPred2 = model.matrix(y ~ ., data=dfData) %*% iModel.2
+## get aggregate
+iAggregate = cbind(iPred1, iPred2)
+iAggregate = sweep(iAggregate, 2, iMixWeights, '*')
+iAggregate = rowSums(iAggregate)
+iMSE.train2 = mean((iAggregate - dfData$y)^2)
+
+## repeat on the test and validation data
+## calculate training error
+iPred1 = model.matrix(y ~ ., data=dfData.pr) %*% iModel.1
+iPred2 = model.matrix(y ~ ., data=dfData.pr) %*% iModel.2
+## get aggregate
+iAggregate = cbind(iPred1, iPred2)
+iAggregate = sweep(iAggregate, 2, iMixWeights, '*')
+iAggregate = rowSums(iAggregate)
+iMSE.test.pr2 = mean((iAggregate - dfData.pr$y)^2)
+
+## try on the validation set
+## repeat on the test and validation data
+## calculate training error
+iPred1 = model.matrix(y ~ ., data=dfData.val[,colnames(dfData.pr)]) %*% iModel.1
+iPred2 = model.matrix(y ~ ., data=dfData.val[,colnames(dfData.pr)]) %*% iModel.2
+## get aggregate
+iAggregate = cbind(iPred1, iPred2)
+iAggregate = sweep(iAggregate, 2, iMixWeights, '*')
+iAggregate = rowSums(iAggregate)
+iMSE.val2 = mean((iAggregate - dfData.val$y)^2)
+
+###########################################################
+#### 3 component distribution
+###########################################################
+dfData = dfData.train.rf1
+dim(dfData)
+## take a subset of the training data to fit model
+i = sample(1:nrow(dfData), size = nrow(dfData)*0.10, replace = F)
+dfData.pr = dfData[-i,]
+dfData = dfData[i,]
+## make model matrix and fit model
+m = model.matrix(y ~ ., data=dfData)
+dim(m)
+stanDso = rstan::stan_model(file='mercedezBenzGrMan/studentTMixtureRegression3Components.stan')
+
+lStanData = list(Ntotal=nrow(dfData), y=dfData$y, iMixtures=3, Ncol=ncol(m), X=m)
+
+## give initial values if you want, look at the density plot 
+initf = function(chain_id = 1) {
+  list(mu = c(75, 90, 120), sigma = c(5, 11, 11*2), iMixWeights=c(0.3, 0.3, 0.4), nu=c(3, 3, 3))
+} 
+
+fit.stan3 = sampling(stanDso, data=lStanData, iter=1200, chains=1, init=initf, cores=1, pars=c('sigma', 'iMixWeights', 'betasMix1',
+                                                                                              'betasMix2', 'betasMix3', 'mu', 'nu',
+                                                                                              'betaSigma'))
+print(fit.stan3, digi=3)
+#traceplot(fit.stanTMix)
+
+## get fitted values
+m = extract(fit.stan3)
+names(m)
+
+## get the coefficients
+iModel.1 = c(mean(m$mu[,1]), apply(m$betasMix1, 2, mean))
+iModel.2 = c(mean(m$mu[,2]), apply(m$betasMix2, 2, mean))
+iModel.3 = c(mean(m$mu[,3]), apply(m$betasMix3, 2, mean))
+
+iMixWeights = apply(m$iMixWeights, 2, mean)
+
+## calculate training error
+iPred1 = model.matrix(y ~ ., data=dfData) %*% iModel.1
+iPred2 = model.matrix(y ~ ., data=dfData) %*% iModel.2
+iPred3 = model.matrix(y ~ ., data=dfData) %*% iModel.3
+
+## get aggregate
+iAggregate = cbind(iPred1, iPred2, iPred3)
+iAggregate = sweep(iAggregate, 2, iMixWeights, '*')
+iAggregate = rowSums(iAggregate)
+iMSE.train3 = mean((iAggregate - dfData$y)^2)
+
+## repeat on the test and validation data
+## calculate training error
+iPred1 = model.matrix(y ~ ., data=dfData.pr) %*% iModel.1
+iPred2 = model.matrix(y ~ ., data=dfData.pr) %*% iModel.2
+iPred3 = model.matrix(y ~ ., data=dfData.pr) %*% iModel.3
+## get aggregate
+iAggregate = cbind(iPred1, iPred2, iPred3)
+iAggregate = sweep(iAggregate, 2, iMixWeights, '*')
+iAggregate = rowSums(iAggregate)
+iMSE.test.pr3 = mean((iAggregate - dfData.pr$y)^2)
+
+## try on the validation set
+## repeat on the test and validation data
+## calculate training error
+iPred1 = model.matrix(y ~ ., data=dfData.val[,colnames(dfData.pr)]) %*% iModel.1
+iPred2 = model.matrix(y ~ ., data=dfData.val[,colnames(dfData.pr)]) %*% iModel.2
+iPred3 = model.matrix(y ~ ., data=dfData.val[,colnames(dfData.pr)]) %*% iModel.3
+## get aggregate
+iAggregate = cbind(iPred1, iPred2, iPred3)
+iAggregate = sweep(iAggregate, 2, iMixWeights, '*')
+iAggregate = rowSums(iAggregate)
+iMSE.val3 = mean((iAggregate - dfData.val$y)^2)
 
 
 
---
+++ mercedezBenzGrMan/studentTMixtureRegression3Components.stan
@@ -0,0 +1,59 @@
+data {
+    int<lower=1> Ntotal; // number of observations
+    real y[Ntotal]; // response variable - normally distributed
+    int<lower=2> iMixtures; // number of mixture distributions
+    int<lower=1> Ncol; // total number of columns in model matrix
+    matrix[Ntotal, Ncol] X; // model matrix
+}
+transformed data{
+  real betaShape;
+  real betaRate;
+  betaShape = 0.5;
+  betaRate = 1e-4;
+}
+parameters { // the parameters to track
+    real<lower=0> sigma[iMixtures]; // scale parameters for normal distribution  
+    simplex[iMixtures] iMixWeights; // weights for the number of mixtures (should sum to one)
+    vector[(Ncol-1)] betasMix1; // regression parameters for each mixture component
+    vector[(Ncol-1)] betasMix2; // regression parameters for each mixture component
+    vector[(Ncol-1)] betasMix3; // regression parameters for each mixture component
+    ordered[iMixtures] mu; // ordered intercept
+    real<lower=1> nu[iMixtures]; // degrees of freedom parameter
+    real<lower=0.1> betaSigma[iMixtures]; // standard deviation parameter for the joint prior for betas
+  }
+transformed parameters { // calculated parameters
+    vector[Ntotal] muMix1; // number of fitted values
+    vector[Ntotal] muMix2; // number of fitted values
+    vector[Ntotal] muMix3; // number of fitted values
+    matrix[Ntotal, (Ncol-1)] mX2; // new model matrix without intercept
+    mX2 = X[,2:Ncol]; 
+    // calculate fitted values without intercept
+    muMix1 = mX2 * betasMix1;
+    muMix2 = mX2 * betasMix2;
+    muMix3 = mX2 * betasMix3;
+}
+model {
+  // see stan manual page 187 for an example
+  real ps[iMixtures]; // temporary variable for log components
+  // any priors go here, get some information from ones we fit earlier
+  nu ~ exponential(1/29.0);
+  mu[1] ~ normal(70, 10);
+  mu[2] ~ normal(90, 10);
+  mu[3] ~ normal(120, 10);
+  betasMix1 ~ normal(0, betaSigma[1]);
+  betasMix2 ~ normal(0, betaSigma[2]);
+  betasMix3 ~ normal(0, betaSigma[3]);
+  // prior for standard deviations of coefficients
+  betaSigma ~ gamma(betaShape, betaRate);
+  sigma ~ cauchy(0, 2.5); // weak prior
+  iMixWeights ~ dirichlet(rep_vector(2.0, iMixtures));
+  // loop to calculate likelihood
+  for(n in 1:Ntotal){
+    // second loop for number of mixture components
+    ps[1] = log(iMixWeights[1]) + student_t_lpdf(y[n] | nu[1], mu[1]+muMix1[n], sigma[1]);
+    ps[2] = log(iMixWeights[2]) + student_t_lpdf(y[n] | nu[2], mu[2]+muMix2[n], sigma[2]);
+    ps[3] = log(iMixWeights[3]) + student_t_lpdf(y[n] | nu[3], mu[3]+muMix3[n], sigma[3]);
+    target += log_sum_exp(ps);
+  }
+}
+
